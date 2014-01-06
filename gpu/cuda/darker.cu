bcf142ddd81103126b42009e7a10c173f9b7b674
Linus Schoemaker <linus@clipperhq.com>
2014-01-06 21:09:57 +0100

    Try to improve darker.cu

--- gpu/cuda/darker.cu
+++ gpu/cuda/darker.cu
@@ -10,24 +10,28 @@ using std::endl;
 using std::fixed;
 using std::setprecision;
 
+const unsigned int nrThreads = 256;
+
 __global__ void darkenImage(const unsigned char * inputImage, 
     unsigned char * outputImage, const int width, const int height){
-  int x = threadIdx.x;
-  int y = threadIdx.y;
+  int x = (blockIdx.x * blockDim.x) + threadIdx.x;
+  int y = (blockIdx.y * blockDim.y) + threadIdx.y;
+  if(x < width && y < height){
 
-  float grayPix = 0.0f;
+    float grayPix = 0.0f;
 
-  float r = 
-    static_cast< float >(inputImage[(y * width) + x]);
-  float g = 
-    static_cast< float >(inputImage[(width * height) + (y * width) + x]);
-  float b = 
-    static_cast< float >(inputImage[(2 * width * height) + (y * width) + x]);
+    float r = 
+      static_cast< float >(inputImage[(y * width) + x]);
+    float g = 
+      static_cast< float >(inputImage[(width * height) + (y * width) + x]);
+    float b = 
+      static_cast< float >(inputImage[(2 * width * height) + (y * width) + x]);
 
-  grayPix = ((0.3f * r) + (0.59f * g) + (0.11f * b));
-  grayPix = (grayPix * 0.6f) + 0.5f;
+    grayPix = ((0.3f * r) + (0.59f * g) + (0.11f * b));
+    grayPix = (grayPix * 0.6f) + 0.5f;
 
-  outputImage[(y * width) + x] = static_cast< unsigned char >(grayPix);
+    outputImage[(y * width) + x] = static_cast< unsigned char >(grayPix);
+  }
 }
 
 
@@ -35,33 +39,59 @@ __global__ void darkenImage(const unsigned char * inputImage,
 void darkGray(const int width, const int height, 
     const unsigned char * inputImage, unsigned char * darkGrayImage) {
 	NSTimer kernelTime = NSTimer("darker", false, false);
-
+  cudaError_t devRetVal = cudaSuccess;
+  int color_image_size = width*height*3;
+  int bw_image_size = width*height;
 
   
   //Allocate vectors in device memory
-  int color_image_size = width*height*3;
-  int bw_image_size = width*height;
   unsigned char * d_input;
-  cudaMalloc(&d_input, color_image_size);
+  if( (devRetVal = cudaMalloc(&d_input, color_image_size * 
+          sizeof(unsigned char))) != cudaSuccess){
+    cerr << "Impossible to allocate device memory for d_input." << endl;
+    exit(1);
+  }
   unsigned char * d_output;
-  cudaMalloc(&d_output, bw_image_size);
+  if( (devRetVal = cudaMalloc(&d_output, bw_image_size*sizeof(unsigned char)))
+      != cudaSuccess){
+    cerr << "Impossible to allocate device memory for d_output." << endl;
+    exit(1);
+  }
+
 
   //Copy vector from host memory to device memory
-  cudaMemcpy(d_input, inputImage, color_image_size , cudaMemcpyHostToDevice);
-  cudaMemcpy(d_output, darkGrayImage, bw_image_size , cudaMemcpyHostToDevice);
+  if( (devRetVal = cudaMemcpy(d_input, inputImage, color_image_size , 
+          cudaMemcpyHostToDevice)) != cudaSuccess){
+    cerr << "Impossible to copy inputImage to device" << endl;
+    exit(1);
+  }
+  if( (devRetVal = cudaMemcpy(d_output, darkGrayImage, bw_image_size , 
+          cudaMemcpyHostToDevice)) != cudaSuccess){
+    cerr << "Impossible to copy darkGrayImage to device" << endl;
+    exit(1);
+  }
 
+  dim3 threadsPerBlock(32,32);
+  dim3 numBlocks(color_image_size/threadsPerBlock.x, color_image_size/threadsPerBlock.y);
 
-
-
-  int numBlocks = 1;
-  dim3 threadsPerBlock(width,height);
 	kernelTime.start();
   darkenImage<<<numBlocks, threadsPerBlock>>>(d_input, d_output, width,
       height);
+  cudaDeviceSynchronize();
 	kernelTime.stop();
 
+  if ( ( devRetVal = cudaGetLastError()) != cudaSuccess ) {
+    cerr << "Kernel has some kind of issue: " << cudaGetErrorString(devRetVal)
+      << endl;
+    exit(1);
+  }
+
   //Copy vector from device memory to host memory
-  cudaMemcpy(darkGrayImage, d_output, bw_image_size, cudaMemcpyDeviceToHost);
+  if ( (devRetVal = cudaMemcpy(darkGrayImage, d_output, bw_image_size, 
+          cudaMemcpyDeviceToHost)) != cudaSuccess){
+    cerr << "Impossible to copy d_output to host " << endl;
+    exit(1);
+  }
 
   cudaFree(d_input);
   cudaFree(d_output);
